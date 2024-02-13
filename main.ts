480d9dad3f7374460046cd290f83493901943f4c
altrlu <159902688+altrlu@users.noreply.github.com>
2024-02-13 11:50:50 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let x = 0
 function Linienzeichnen (num: number) {
     for (let Index = 0; Index <= 4; Index++) {
         led.plot(num, Index)
@@ -8,5 +9,16 @@ function Linienzeichnen (num: number) {
     }
 }
 basic.forever(function () {
-	
+    for (let x = 0; x <= 4; x++) {
+        led.plot(x, 0)
+        basic.pause(100)
+        led.unplot(x, 0)
+    }
+    x = 3
+    for (let index = 0; index < 3; index++) {
+        led.plot(x, 0)
+        basic.pause(100)
+        led.unplot(x, 0)
+        x += -1
+    }
 })
